d11e0c41e2e9ca44115e8d6343d0d10da6c21d35
Thailan <thailan-higor@hotmail.com>
2017-12-16 19:25:13 -0200

    Implementando a verificacao de xeque

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -16,6 +16,10 @@
             qtdMovimentos++;
         }
 
+        public void decrementarQteMovimentos() {
+            qtdMovimentos--;
+        }
+
         public bool existeMovimentosPossiveis() {
             bool[,] mat = movimentosPossiveis();
             for(int i = 0; i < tab.linhas; i++) {
